a7dfbb374a416bbfd0e6ae3a7c2cc502f6bd61d7
hofstede-matheus <hofstede.matheus@gmail.com>
2025-07-21 21:39:05 +0200

    feat: update agent name and description to generate AWS certification questions from YouTube videos

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ model = BedrockModel(
 )
 
 agent = Agent(
-    name="Calculator",
-    description="A calculator agent that can perform basic arithmetic operations",
+    name="Generate AWS certification-like questions from a YouTube video",
+    description="A agent that can generate AWS certification-like questions from a YouTube video",
     tools=[download_youtube_audio],
     model=model,
 )
